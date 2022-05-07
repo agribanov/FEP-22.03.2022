dbe4cb9c7e83003c0fc19e2c22c62e6eefade15d
Aleksandr Gribanov <aleksandr.gribanov@gmail.comD>
2022-05-07 13:32:31 +0300

    Lection done

--- lection/app.js
+++ lection/app.js
@@ -1,118 +1,32 @@
-const DELETE_BTN_CLASS = 'delete-btn';
-const CONTACT_ROW_SELECTOR = '.contact-row';
+'use strict';
 
-const contactForm = document.querySelector('#newContactForm');
-const contactsListEl = document.querySelector('#contactsList');
-const contactTemplate = document.querySelector('#contactTemplate').innerHTML;
-const formInputs = document.querySelectorAll('.form-input');
-
-contactForm.addEventListener('submit', onContactFormSubmit);
-contactsListEl.addEventListener('click', onContactsListClick);
-
-let contactsList = [
-    {
-        id: 100,
-        name: 'John1',
-        surname: 'Doe',
-        phone: '5555',
-    },
-    {
-        id: 200,
-        name: 'John2',
-        surname: 'Doe',
-        phone: '5555',
-    },
-    {
-        id: 300,
-        name: 'John3',
-        surname: 'Doe',
-        phone: '5555',
-    },
-];
-
-init();
-
-function onContactFormSubmit(e) {
-    e.preventDefault();
-
-    const newContact = getContact();
-
-    if (isContactValid(newContact)) {
-        addContact(newContact);
-        resetForm();
-    } else {
-        alert('Not valid');
-    }
+function Person(name) {
+    this.name = name;
+    this.age = 55;
 }
 
-function onContactsListClick(e) {
-    if (e.target.classList.contains(DELETE_BTN_CLASS)) {
-        const id = getContactId(e.target);
-        removeContact(id);
-    }
+Person.prototype.sayHi = function () {
+    console.log('hello ' + this.name);
+};
+
+function Student(name) {
+    this.name = name;
 }
 
-function init() {
-    renderList();
+Student.prototype = new Person('Hacked');
+Student.prototype.study = function () {
+    console.log('I am studying');
+};
+
+function Magistr(name) {
+    this.name = name;
+    this.grant = 'Magistr';
 }
 
-function getContact() {
-    const contact = {};
+Magistr.prototype = new Student();
 
-    formInputs.forEach((inp) => {
-        contact[inp.name] = inp.value;
-    });
+const alice = new Person('Alice');
 
-    return contact;
-}
-
-function isContactValid(contact) {
-    return (
-        isTextFieldValid(contact.name) &&
-        isTextFieldValid(contact.surname) &&
-        isPhoneFieldValid(contact.phone)
-    );
-}
-
-function isTextFieldValid(value) {
-    return value !== '';
-}
-
-function isPhoneFieldValid(value) {
-    return isTextFieldValid(value) && !isNaN(value);
-}
-
-function generateContactHtml(contact) {
-    return interpolate(contactTemplate, contact);
-}
-
-function addContact(contact) {
-    // contact.id = Math.random();
-    contact.id = Date.now();
-    contactsList.push(contact);
-
-    renderList();
-}
-
-function renderList() {
-    contactsListEl.innerHTML = contactsList.map(generateContactHtml).join('\n');
-}
-
-function resetForm() {
-    contactForm.reset();
-}
-
-function getContactId(el) {
-    const contactRowEl = el.closest(CONTACT_ROW_SELECTOR);
-
-    return +contactRowEl.dataset.contactId;
-}
-
-function removeContact(id) {
-    // const index = contactsList.findIndex((obj) => obj.id === id);
-    // contactsList.splice(index, 1)
-
-    contactsList = contactsList.filter((obj) => obj.id !== id);
-
-    renderList();
-}
+const alex = new Student('Alex');
+const bob = new Student('bob');
+const john = new Magistr('John');
