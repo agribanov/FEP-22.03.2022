7837d0a1efcce80d17a6952afdd981d629a9ff13
Aleksandr Gribanov <aleksandr.gribanov@gmail.comD>
2022-05-10 21:27:12 +0300

    Lection done

---
+++ dz13/scripts.js
@@ -0,0 +1,19 @@
+function Calculator(base) {
+    this.result = base;
+}
+
+Calculator.prototype.add = function (b) {
+    return (this.result += b);
+};
+Calculator.prototype.sub = function (b) {
+    return (this.result -= b);
+};
+Calculator.prototype.mult = function (b) {
+    return (this.result *= b);
+};
+Calculator.prototype.div = function (b) {
+    return (this.result /= b);
+};
+Calculator.prototype.set = function (b) {
+    return (this.result = b);
+};
--- lection/app.js
+++ lection/app.js
@@ -1,32 +1,133 @@
+// Наследование
+// Инкапсуялция
+// Полиморфизм
+
+// public
+// private
+
 'use strict';
 
-function Person(name) {
-    this.name = name;
-    this.age = 55;
-}
+// function Person(name) {
+//     this._name = name;
+//     this.age = 55;
+// }
 
-Person.prototype.sayHi = function () {
-    console.log('hello ' + this.name);
-};
+// Person.prototype.setName = function (name) {
+//     if (name && typeof name === 'string') {
+//         this._name = name;
+//     }
+// };
 
-function Student(name) {
-    this.name = name;
-}
+// Person.prototype.getName = function () {
+//     return this._name;
+// };
+// Person.prototype._formatMessage = function () {
+//     return 'hello ' + this._name;
+// };
 
-Student.prototype = new Person('Hacked');
-Student.prototype.study = function () {
-    console.log('I am studying');
-};
+// Person.prototype.sayHi = function () {
+//     console.log(this._formatMessage());
+// };
 
-function Magistr(name) {
-    this.name = name;
-    this.grant = 'Magistr';
-}
+// class Person {
+//     constructor(name) {
+//         this._name = name;
+//         this._surname = 'Smith';
+//     }
 
-Magistr.prototype = new Student();
+//     get fullname() {
+//         return this._name + ' ' + this._surname;
+//     }
 
-const alice = new Person('Alice');
+//     set fullname(newName) {
+//         if (!newName && typeof newName !== 'string') return newName;
 
-const alex = new Student('Alex');
-const bob = new Student('bob');
-const john = new Magistr('John');
+//         const [name, surname] = newName.split(' ');
+
+//         this._name = name;
+//         this._surname = surname;
+//     }
+
+//     _formatMessage() {
+//         return 'hello ' + this._name;
+//     }
+
+//     // setName(name) {
+//     //     if (name && typeof name === 'string') {
+//     //         this._name = name;
+//     //     }
+//     // }
+
+//     // getName() {
+//     //     return this._name;
+//     // }
+
+//     sayHi() {
+//         console.log(this._formatMessage());
+//     }
+// }
+
+// class Student extends Person {
+//     _formatMessage() {
+//         return 'Hello, I am student: ' + this._name + this.type;
+//     }
+
+//     study() {
+//         console.log('I am studying');
+//     }
+
+//     sayHi() {
+//         console.log('I am student');
+
+//         super.sayHi();
+//         super.sayHi();
+//         super.sayHi();
+//     }
+// }
+
+// Person.prototype.setName = function (name) {
+//     if (name && typeof name === 'string') {
+//         this._name = name;
+//     }
+// };
+
+// Person.prototype.getName = function () {
+//     return this._name;
+// };
+
+// function Student(name) {
+//     this._name = name;
+// }
+
+// Student.prototype = new Person();
+// Student.prototype._formatMessage = function () {
+//     return 'Hello, I am student: ' + this._name;
+// };
+
+// Student.prototype.study = function () {
+//     console.log('I am studying');
+// };
+
+// function Magistr(name) {
+//     this._name = name;
+// }
+
+// // extend(Magistr, Person, Student);
+
+// // extend(Student, Person);
+
+// function extend(Child, ...Parents) {
+//     Parents.forEach((Parent) => {
+//         for (const key in Parent.prototype) {
+//             Child.prototype[key] = Parent.prototype[key];
+//         }
+//     });
+// }
+
+// const alice = new Person('Alice');
+// const alex = new Student('Alex');
+// // const bob = new Magistr('bob');
+
+// function greeting(obj) {
+//     obj.sayHi();
+// }
