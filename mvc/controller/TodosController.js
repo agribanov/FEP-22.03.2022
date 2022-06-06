bca2aff07acca91318d58a906bf3702651e0b9a2
Aleksandr Gribanov <aleksandr.gribanov@gmail.comD>
2022-06-06 10:58:36 +0300

    Lection done

---
+++ mvc/controller/TodosController.js
@@ -0,0 +1,25 @@
+class TodosController {
+    constructor($container) {
+        this._todosListView = new TodosListView({
+            onToggle: (id) => this.toggleTodo(id),
+            onDelete: (id) => this.removeTodo(id),
+        });
+
+        $container.append(this._todosListView.$el);
+
+        this._todosList = new TodosCollection();
+        this._todosList
+            .fetchList()
+            .then(() => this._todosListView.renderList(this._todosList.list));
+    }
+
+    toggleTodo(id) {
+        this._todosList.toggleTodo(id);
+        this._todosListView.renderList(this._todosList.list);
+    }
+
+    removeTodo(id) {
+        this._todosList.removeTodo(id);
+        this._todosListView.renderList(this._todosList.list);
+    }
+}
---
+++ mvc/model/TodosCollection.js
@@ -0,0 +1,39 @@
+const TODOS_URL = 'https://5dd3d5ba8b5e080014dc4bfa.mockapi.io/todos/';
+
+class TodosCollection {
+    constructor() {
+        this.list = [];
+    }
+
+    fetchList() {
+        return fetch(TODOS_URL)
+            .then((res) => res.json())
+            .then((data) => (this.list = data));
+    }
+
+    toggleTodo(todoId) {
+        const todoItem = this.list.find(({ id }) => id == todoId);
+
+        if (!todoItem) {
+            return console.error('Id not found', todoId);
+        }
+
+        todoItem.isDone = !todoItem.isDone;
+
+        fetch(TODOS_URL + todoId, {
+            method: 'PUT',
+            body: JSON.stringify(todoItem),
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+    }
+
+    removeTodo(todoId) {
+        this.list = this.list.filter(({ id }) => id != todoId);
+
+        fetch(TODOS_URL + todoId, {
+            method: 'DELETE',
+        });
+    }
+}
