e2d4d418f0aa7245050cd15f7358f1a103329490
Aleksandr Gribanov <aleksandr.gribanov@gmail.comD>
2022-06-14 19:53:29 +0300

    Home work

---
+++ dz22/app.js
@@ -0,0 +1,10 @@
+// const calc = require('./calculator');
+
+// console.log(calc.set(10));
+// console.log(calc.add(4));
+
+// const Calculator = require('./calculator/Calculator');
+
+// const calc = new Calculator();
+
+const calc = require('./calculator/calculator');
